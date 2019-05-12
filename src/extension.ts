dbd03d8f262989d4ec992f4270eaa4b3b6269e9f
Benjamin Cooper <benjaminkagancooper@gmail.com>
2019-05-11 22:59:37 -0400

    compiles list of changed branch files

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,57 @@
 import * as vscode from 'vscode';
+import * as g from 'isomorphic-git';
+import * as SimplGit from 'simple-git/promise';
+import * as fs from 'fs';
 
-export function activate(context: vscode.ExtensionContext) {
+g.plugins.set('fs', fs);
+
+const checkForGitRepo = async(): Promise<string | null> => {
+
+  if (vscode.workspace.workspaceFolders) {
+    try {
+      const rootFolderPath = await g.findRoot({ filepath: vscode.workspace.workspaceFolders[0].uri.fsPath });
+
+      return rootFolderPath;
+    } catch (err) {
+      return null;
+    }
+  }
+  return null;
+};
+
+export const activate = async (context: vscode.ExtensionContext) => {
   console.log('Congratulations, your extension "open-branch-files" is now active!');
-  const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World!');
+  const disposable = vscode.commands.registerCommand('extension.openBranchFiles', async () => {
+    const repo = await checkForGitRepo();
+    if (repo) {
+      vscode.window.showInformationMessage(`Opening branch files for folder path ${repo}`);
+      const filesToOpen: string[] = [];
+
+      const git = SimplGit(repo);
+      const diff = await git.diffSummary(['master...']);
+      const status = await git.status();
+
+      for (const unstagedFile of status.files) {
+        if (!filesToOpen.includes(unstagedFile.path)) {
+          filesToOpen.push(unstagedFile.path);
+        }
+      }
+
+      for (const committedFile of diff.files) {
+        if (!filesToOpen.includes(committedFile.file)) {
+          filesToOpen.push(committedFile.file);
+        }
+      }
+
+      filesToOpen.map((filePath) => { console.log(filePath); });
+
+    } else {
+      vscode.window.showInformationMessage('You are not currently working in a Git repository');
+    }
+
   });
 
   context.subscriptions.push(disposable);
-}
+};
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
